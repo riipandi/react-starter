faa6bdce48b634eb66a293a244c65da5ae3fa08d
Tim Ruhay <timruhay@gmail.com>
2018-12-26 16:48:14 +0700

    Initial commit

---
+++ docs/precache-manifest.abe125e0c326bce238ad2ef8fbbb8c89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4453f0a7fb703ea10957",
+    "url": "/static/js/main.4453f0a7.chunk.js"
+  },
+  {
+    "revision": "1d22965687f9520c933a",
+    "url": "/static/js/1.1d229656.chunk.js"
+  },
+  {
+    "revision": "4453f0a7fb703ea10957",
+    "url": "/static/css/main.59d0217b.chunk.css"
+  },
+  {
+    "revision": "234974a9868e4ce42ec4a971004d713d",
+    "url": "/index.html"
+  }
+];
